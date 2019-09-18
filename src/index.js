73c9464c8b3d84f77588344763be95a284c0da4c
mcforte <fortemichaelc@gmail.com>
2019-09-18 09:48:06 +0800

    first commit

---
+++ src/index.js
@@ -0,0 +1,8 @@
+import {styeBody, addTitle, contact} from './dom';
+import users, { getPremUsers } from './data';
+
+const premUsers = getPremUsers(users);
+console.log(users);
+console.log(premUsers);
+
+console.log('test3');
